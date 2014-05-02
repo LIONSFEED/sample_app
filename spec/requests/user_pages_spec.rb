e1085052ce187a415b938f07bea14e94003a97f4
Andrew Hamilton <www.andrewhamilton@gmail.com>
2014-05-02 21:46:06 +0800

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -8,16 +8,15 @@ describe "User pages" do
     before { visit signup_path }
 
     it { should have_selector('h1',    text: 'Sign up') }
-    it { should have_selector('title', text: full_title('Sign up')) }
+    it { should have_selector('title', text: 'Sign up') }
   end
 
   describe "profile page" do
-  	let(:user) { FactoryGirl.create(:user) }
-  	# Code to make a user variable
-  	before { visit user_path(user) }
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
 
-  	it { should have_selector('h1',    text: user.name) }
-  	it { should have_selector('title', text: user.name) }
+    it { should have_selector('h1',    text: user.name) }
+    it { should have_selector('title', text: user.name) }
   end
 
   describe "signup" do
@@ -53,10 +52,12 @@ describe "User pages" do
 
       describe "after saving the user" do
         before { click_button submit }
+
         let(:user) { User.find_by_email('user@example.com') }
 
         it { should have_selector('title', text: user.name) }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_link('Sign out') }
       end
     end
   end
